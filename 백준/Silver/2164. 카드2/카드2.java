9fb5644f1445de83bd988f0306ad5f5d3a22bf2e
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-08-18 19:24:53 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,28 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();
+        int num = 2;
+        int ans;
+        // num 전에 있는 2의 제곱수를 구하기 위한 2제곱
+        while(num < N){
+            num *= 2;
+        }
+        // /2를 하여 전에 있는 2의 제곱수 구함
+        num = num / 2;
 
-        for(int i = 1; i < N + 1; i++){
-            queue.offer(i);
+        // 구하려는 값이 2보다 작을 경우 N - 이전 2의 제곱수 * 2의 값
+        if(N > 2){
+            ans = (N - num) * 2;
+            sb.append(ans);
         }
-        while(queue.size() > 1){
-            queue.poll();
-            queue.offer(queue.poll());
+        // 1 혹은 2의 값은 그대로 1, 2
+        else{
+            sb.append(N);
         }
-        sb.append(queue.poll());
         System.out.println(sb);
     }
 }
