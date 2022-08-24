6950d8600bf185a63c14aad6052dd423f407081d
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-08-24 17:34:29 +0900

    [D2] Title: 숫자 배열 회전, Time: 110 ms, Memory: 18,376 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,49 +1,63 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int [][]  rotate (int[][] arr){
-        int [][] rotated = new int [arr.length][arr.length];
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length; j++){
-                // 90 도로 돌리기 위해 3번째 줄 ([2]) 값의 첫 번째 값이 맨 위 왼쪽으로. 그 다음 2번째 줄 [1] 값의 첫 번째 값이 맨 위 가운데로
-                rotated[i][j] = arr[arr.length - j - 1][i];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("#" + tc + "\n");
+            int N = Integer.parseInt(br.readLine());
+
+            // N 크기 만큼의 2차원 배열
+            int [][] num = new int[N][N];
+            for(int i = 0; i < N; i++){
+                st = new StringTokenizer(br.readLine());
+                // 2차원 배열 받기
+                for(int j = 0; j < N; j++){
+                    num[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            // 함수를 통해 90도를 받고 180도 270도는 90도를 90도 하고 180도를 90도 하면 된다.
+            int [][] rotated90 = rotated90(num);
+            int [][] rotated180 = rotated90(rotated90);
+            int [][] rotated270 = rotated90(rotated180);
+
+            // 90도 , 180도, 270 도 돌린 값들의 위에 행부터 " " 씩 띄워서 보여줘야 한다.
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    sb.append(rotated90[i][j]);
+                }
+                sb.append(" ");
+                for(int j = 0; j < N; j++){
+                    sb.append(rotated180[i][j]);
+                }
+                sb.append(" ");
+                for(int j = 0; j < N; j++){
+                    sb.append(rotated270[i][j]);
+                }
+                sb.append("\n");
+            }
+
+            System.out.print(sb);
+        }
+    }
+
+    // 90도 돌리는 함수
+    static int[][] rotated90 (int [][]num){
+        // 90 도 돌아진 값을 받을 2차원 배열
+        int [][] rotated = new int[num.length][num.length];
+        // 90도 돌리려면 아래에 있는 열부터 읽어서 행렬에 채우면 된다.
+        for(int i = 0; i < num.length; i++){
+            for(int j = 0; j < num.length; j++){
+                rotated[i][j] = num[num.length - 1 - j][i];
             }
         }
         return rotated;
     }
-
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for(int tc = 1; tc <= T; tc++){
-            int N = sc.nextInt();
-            int [][] num = new int[N][N];
-
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < N; j++){
-                    num[i][j] = sc.nextInt();
-                }
-            }
-            int[][] rotated90 = rotate(num);
-            int[][] rotated180 = rotate(rotated90);
-            int[][] rotated270 = rotate(rotated180);
-            System.out.println("#"+tc);
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < N; j++){
-                    System.out.print(rotated90[i][j]);
-                }
-                System.out.print(" ");
-                for(int j = 0; j < N; j++){
-                    System.out.print(rotated180[i][j]);
-                }
-                System.out.print(" ");
-                for(int j = 0; j < N; j++){
-                    System.out.print(rotated270[i][j]);
-                }
-                System.out.println();
-            }
-        }
-    }
 }
+
