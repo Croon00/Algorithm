104062196e1ff189a5d0a68809c39dc55044a934
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-08-25 23:24:17 +0900

    [D3] Title: 햄버거 다이어트, Time: 174 ms, Memory: 20,900 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,20 +4,20 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int N, L;
-    static int maxHamburger;
-    static int[] taste, calorie;
+    // static으로 재료 갯수, 제한칼로리, 가장 맛있은 조합 맛 받기
+    static int N, L, maxTaste;
+    // 각 재료에 대한 맛이랑 칼로리
+    static int []  taste, calorie;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("#" + tc + " ");
+        for(int tc = 1; tc <= T; tc++){
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             L = Integer.parseInt(st.nextToken());
-            maxHamburger = 0;
+            maxTaste = 0;
+            // N 크기만큼 선언
             taste = new int[N];
             calorie = new int[N];
             for(int i = 0; i < N; i++){
@@ -26,23 +26,23 @@ public class Solution {
                 calorie[i] = Integer.parseInt(st.nextToken());
             }
             rec(0, 0, 0);
-            System.out.println("#" + tc + " " + maxHamburger);
+            System.out.println("#" + tc + " " + maxTaste);
         }
     }
-
-    static void rec(int n, int sum_taste, int sum_calorie ){
-
-        if(sum_calorie > L){
+    static void rec(int idx, int sumTaste, int sumCalorie){
+        // L 이상일땐 칼로리 제한을 넘음으로 return해서 해당 재귀함수 끝내기
+        if(sumCalorie > L){
             return;
         }
-        if(n == N){
-            if(maxHamburger < sum_taste){
-                maxHamburger = sum_taste;
+        // N번째 재료까지 봤을 때 maxTaste 값을 가장 맛있을 때로 변경 후 재귀함수 끝내기
+        if(idx == N){
+            if(maxTaste < sumTaste){
+                maxTaste = sumTaste;
             }
             return;
         }
-
-        rec(n + 1,sum_taste + taste[n], sum_calorie + calorie[n]);
-        rec(n + 1, sum_taste, sum_calorie);
+        // idx를 증가시키면서 해당 재료의 맛과 칼로리를 더하거나 안 더하는 방법으로 재귀함수
+        rec(idx + 1, sumTaste+taste[idx], sumCalorie+calorie[idx]);
+        rec(idx + 1, sumTaste, sumCalorie);
     }
 }
