5ff5631f14bf821569e2a8e6be9423b103e9a4ae
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-09-07 09:25:13 +0900

    [Bronze II] Title: 시각, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/18312. 시각/시각.java
+++ 백준/Bronze/18312. 시각/시각.java
@@ -10,15 +10,26 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        int hour = 0;
+        int min = 0;
+        int sec = 0;
         int cnt = 0;
-        for(int i = 0; i <= N; i++){
-            for(int j = 0; j < 60; j++){
-                for(int k = 0; k < 60; k++){
-                    if(i / 10 == K || i % 10 == K || j / 10 == K || j % 10 == K || k / 10 == K || k % 10 == K){
-                        cnt++;
-                    }
+        while (true){
+            if(sec == 60){
+                min++;
+                sec = 0;
+                if(min == 60){
+                    hour++;
+                    min = 0;
                 }
             }
+            if(hour == N + 1){
+                break;
+            }
+            if(hour % 10 == K || hour / 10 == K || min % 10 == K || min / 10 == K || sec % 10 == K || sec / 10 == K){
+                cnt++;
+            }
+            sec++;
         }
         System.out.println(cnt);
     }
