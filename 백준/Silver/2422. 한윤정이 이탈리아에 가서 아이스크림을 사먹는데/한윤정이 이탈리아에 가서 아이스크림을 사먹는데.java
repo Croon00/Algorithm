1aaad4d911478ccbdba7aa9cc64167080ded7cc8
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-09-09 23:11:46 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 232 ms, Memory: 18772 KB -BaekjoonHub

---
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int [][] noMix = new int[N + 1][N + 1];
+        int ans = 0;
+        // 해당 두 숫자 2차원 배열에 구하기
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int iceCream = Integer.parseInt(st.nextToken());
+            int iceCream2 = Integer.parseInt(st.nextToken());
+            noMix[iceCream][iceCream2] = -1;
+            noMix[iceCream2][iceCream] = -1;
+        }
+        // 3개 조합을 -1 인 경우에는 건너 띄우고 카운트 하기
+        for(int i = 1; i <= N - 2; i++){
+            for(int j = i + 1; j <= N - 1; j++){
+                for(int k = j + 1; k <= N; k++){
+                    if(noMix[i][j] == - 1 || noMix[i][k] == -1 || noMix[j][k] == -1 ){
+                        continue;
+                    }
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
