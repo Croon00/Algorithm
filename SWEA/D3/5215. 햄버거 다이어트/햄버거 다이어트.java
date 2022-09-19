3712b1d6907c26924ac1d3784f864483a2134eed
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-09-19 20:14:27 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,353 ms, Memory: 24,300 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,10 +4,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    // static으로 재료 갯수, 제한칼로리, 가장 맛있은 조합 맛 받기
     static int N, L, maxTaste;
-    // 각 재료에 대한 맛이랑 칼로리
-    static int []  taste, calorie;
+    static int [][] hamburger;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -16,33 +15,37 @@ public class Solution {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             L = Integer.parseInt(st.nextToken());
+            hamburger = new int[N][2];
             maxTaste = 0;
-            // N 크기만큼 선언
-            taste = new int[N];
-            calorie = new int[N];
             for(int i = 0; i < N; i++){
                 st = new StringTokenizer(br.readLine());
-                taste[i] = Integer.parseInt(st.nextToken());
-                calorie[i] = Integer.parseInt(st.nextToken());
+                // 맛 과 칼로리
+                hamburger[i][0] = Integer.parseInt(st.nextToken());
+                hamburger[i][1] = Integer.parseInt(st.nextToken());
+
             }
-            rec(0, 0, 0);
-            System.out.println("#" + tc + " " + maxTaste);
-        }
-    }
-    static void rec(int idx, int sumTaste, int sumCalorie){
-        // L 이상일땐 칼로리 제한을 넘음으로 return해서 해당 재귀함수 끝내기
-        if(sumCalorie > L){
-            return;
-        }
-        // N번째 재료까지 봤을 때 maxTaste 값을 가장 맛있을 때로 변경 후 재귀함수 끝내기
-        if(idx == N){
-            if(maxTaste < sumTaste){
-                maxTaste = sumTaste;
+            int taste, calorie;
+            // 부분 집합의 총 갯수
+            for(int i = 0; i < (1 << N); i++){
+                // 각 부분 집합 마다 고르기 위해 초기화
+                taste = 0;
+                calorie = 0;
+                // 원소가 N개라서 (원소를 검사하는)
+                for(int j = 0; j < N; j++){
+                    if((i & (1 << j)) > 0){
+                        // 해당 원소가 있음
+                        taste += hamburger[j][0];
+                        calorie += hamburger[j][1];
+                        if(calorie > L){
+                            break;
+                        }
+                    }
+                    if(calorie < L && maxTaste < taste){
+                        maxTaste = taste;
+                    }
+                }
             }
-            return;
+            System.out.println("#" + tc + " " +maxTaste);
         }
-        // idx를 증가시키면서 해당 재료의 맛과 칼로리를 더하거나 안 더하는 방법으로 재귀함수
-        rec(idx + 1, sumTaste+taste[idx], sumCalorie+calorie[idx]);
-        rec(idx + 1, sumTaste, sumCalorie);
     }
 }
