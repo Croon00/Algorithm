97ee9605d4299c6c08e9bf5e97c918c19ff89741
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-09-26 14:38:33 +0900

    [Silver III] Title: 늑대와 양, Time: 364 ms, Memory: 41536 KB -BaekjoonHub

--- 백준/Silver/16956. 늑대와 양/늑대와 양.java
+++ 백준/Silver/16956. 늑대와 양/늑대와 양.java
@@ -20,6 +20,7 @@ public class Main {
 
         map = new Character[R][C];
 
+        // map 받기
         for(int i = 0; i < R; i++){
             String s = br.readLine();
             for(int j = 0; j < C; j++){
@@ -27,35 +28,38 @@ public class Main {
             }
         }
 
+        // R, C를 돌면서 늑대를 찾으면 bfs 돌기
         loop:
         for(int i = 0; i < R; i++){
             for(int j = 0; j < C; j++){
                 if(map[i][j] == 'W'){
                     bfs(i,j);
                 }
+                // flag 가 true이면 양이 죽기 때문에 break;
                 if(flag){
                     break loop;
                 }
             }
         }
 
-        for(int i = 0; i < R; i++){
-            for(int j = 0; j < C; j++){
-                sb.append(map[i][j]);
-            }
-            sb.append("\n");
-
-        }
+        // 양이 죽기 때문에 0
         if(flag){
             System.out.println(0);
-
         }
+        // 양이 죽지 않을 때 1, 하고 map을 보여준다.
         else{
+            for(int i = 0; i < R; i++){
+                for(int j = 0; j < C; j++){
+                    sb.append(map[i][j]);
+                }
+                sb.append("\n");
+            }
             System.out.println(1);
             System.out.println(sb);
         }
     }
 
+    // bfs
     static void bfs(int x, int y){
         Queue<Integer> queue = new LinkedList<>();
         queue.add(x);
@@ -69,10 +73,12 @@ public class Main {
                 int nr = r + dir[k][0];
                 int nc = c + dir[k][1];
 
+                // 늑대 주변에 .일 경우 벽으로 가둔다.
                 if(nr >= 0 && nr < R && nc >= 0 && nc < C){
                     if(map[nr][nc] == '.'){
                         map[nr][nc] = 'D';
                     }
+                    // 늑대 주변에 양이 있을 경우 flag= true 하고 끝내기.
                     else if(map[nr][nc] == 'S'){
                         flag = true;
                         return;
