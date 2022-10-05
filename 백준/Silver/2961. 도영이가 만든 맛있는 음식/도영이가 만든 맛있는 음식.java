d906f34e5faac8c3c49b3655c736b8a5942ac76a
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-10-05 17:44:10 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 120 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -31,37 +31,33 @@ public class Main {
             taste[i][1] = bitter;
         }
 
-        dfs(0);
+        dfs(0,0,1,0);
         System.out.println(ans);
     }
 
     // 부분 수열로 구해야 한다.
-    static void dfs(int cnt){
-        // 전부 골랐을 때
-        if(cnt == N){
-            int s = 1;
-            int b = 0;
-            int temp = 0;
-            for(int i = 0; i < N; i++){
-                // 체크 된 재료들만
-                if(visited[i]){
-                    temp++;
-                    s *= taste[i][0];
-                    b += taste[i][1];
-                }
-            }
-            // 재료가 1개 이상이라도 사용 되어야 함.
-            if(temp >= 1){
-                ans = Math.min(ans, Math.abs(s - b));
-            }
+    static void dfs(int idx, int cnt, int s, int b){
+
+        // 재료가 N개 이상일 때
+        if(cnt > N){
             return;
         }
 
-        // 해당 원소를 가져가냐 안 가져가냐
-        visited[cnt] = true;
-        dfs(cnt + 1);
-        visited[cnt] = false;
-        dfs(cnt + 1);
+        // 재료가 적어도 1개 이상일 때
+        if(cnt > 0){
+
+            ans = Math.min(ans, Math.abs(s - b));
+
+        }
+
+        for(int i = idx; i < N; i++){
+            int ss = s * taste[i][0];
+            int bb = b + taste[i][1];
+            dfs(i + 1, cnt + 1, ss, bb);
+            ss = 1;
+            bb = 0;
+
+        }
 
     }
 
