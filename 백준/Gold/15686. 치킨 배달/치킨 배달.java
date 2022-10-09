a1434e5106f8478cd12be21d82e67ed0c3521036
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-10-09 15:40:59 +0900

    [Gold V] Title: 치킨 배달, Time: 176 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -7,10 +6,9 @@ import java.util.StringTokenizer;
 public class Main {
     static int N, M;
     static int [][] map;
-    static ArrayList<xy> chicken;
-    static ArrayList<xy> house;
+    static ArrayList<xy> xy;
     static boolean [] visited;
-    static int ans;
+    static int ans = Integer.MAX_VALUE;
 
     static class xy {
         int r;
@@ -30,27 +28,20 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][N];
-        chicken = new ArrayList<>();
-        house = new ArrayList<>();
-        ans = Integer.MAX_VALUE;
+        xy = new ArrayList<>();
+
 
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                // 2인 경우 치킨 집
                 if(map[i][j] == 2){
-                    chicken.add(new xy(i,j));
-                }
-                // 1인 경우 일반 집
-                else if(map[i][j] == 1){
-                    house.add(new xy(i,j));
+                    xy.add(new xy(i,j));
                 }
             }
         }
 
-        // 고를 치킨 집들 방문처리
-        visited = new boolean[chicken.size()];
+        visited = new boolean[xy.size()];
         dfs(0,0);
         System.out.println(ans);
 
@@ -60,36 +51,33 @@ public class Main {
 
     // 조합으로 치킨 집을 고른다
     static void dfs(int cnt, int idx){
-        // 최대 M개의 치킨집 골랐을 경우
+        // 최대 M개의 치킨 집 골랐을 경우
         if(cnt == M){
             // 도시의 치킨 거리
             int distance = 0;
 
-            for(int i = 0; i < house.size(); i++){
-                int minDist = Integer.MAX_VALUE;
-                for(int j = 0; j < chicken.size(); j++){
-                    // 방문 체크한 치킨집 만
-                    if(visited[j]){
-                        // 거리 계산하고 최고 작은거리로 갱신
-                        int dist = Math.abs(house.get(i).r - chicken.get(j).r) + Math.abs(house.get(i).c - chicken.get(j).c);
-                        minDist = Math.min(minDist, dist);
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    // 집인 경우
+                    if(map[i][j] == 1){
+                        int dis = Integer.MAX_VALUE;
+                        for(int k = 0; k < xy.size(); k++){
+                            // 방문 체크 한 치킨 집 경우에만
+                            if(visited[k]){
+                                dis = Math.min(Math.abs(i - xy.get(k).r) + Math.abs(j - xy.get(k).c), dis);
+                            }
+                        }
+                        distance += dis;
                     }
                 }
-                // 도시의 치킨 거리에 누적해서 더하기
-                distance += minDist;
-                // 구하던 도시의 치킨 거리가 ans 이상인 경우 백트래킹
-                if(ans <= distance){
-                    return;
-                }
-
             }
-
             // 가장 짧은 도시의 치킨 거리
             ans = Math.min(ans, distance);
+
             return;
         }
 
-        for(int i = idx; i < chicken.size(); i++){
+        for(int i = idx; i < xy.size(); i++){
             visited[i] = true;
             dfs(cnt + 1, i + 1);
             visited[i] = false;
