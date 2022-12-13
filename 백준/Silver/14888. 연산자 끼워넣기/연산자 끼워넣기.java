e3d238ca4396e67ce57cc9d8b3e39afb5566f9bf
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-12-13 17:47:53 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 84 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,95 +1,81 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-
-
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-
-    static int N, max, min, nums[], operator[], order[];
-
-    static void input(){
-        FastReader fr = new FastReader();
-
-        N = fr.nextInt();
-        max = Integer.MIN_VALUE;
-        min = Integer.MAX_VALUE;
-
-        nums = new int[N + 1];
-        operator = new int[5];
-        order = new int[N + 1];
-
-        for (int i = 1; i <= N; i++){
-            nums[i] = fr.nextInt();
-        }
-        for(int i = 1; i <= 4; i++){
-            operator[i] = fr.nextInt();
-        }
-    }
-
-
-    // 피연산자 2개와 연산자가 주어졌을 때 계산하는 함수
-    static int calculator(int operand1, int operator, int operand2){
-        if(operator == 1){
-            return operand1 + operand2;
-        }
-        else if(operator == 2){
-            return operand1 - operand2;
-        }
-        else if(operator == 3){
-            return operand1 * operand2;
-        }
-        else{
-            return operand1 / operand2;
-        }
-    }
-
-    static void rec_func(int k, int value){
-        if(k == N){
-            // int value = calculator();
-            max = Math.max(value, max);
-            min = Math.min(value, min);
-
-        }
-        for(int cand = 1; cand <= 4; cand++){
-            if(operator[cand] >= 1){
-                operator[cand]--;
-                order[k] = cand;
-                rec_func(k + 1, calculator(value, cand, nums[k + 1]));
-                operator[cand]++;
-                order[k] = 0;
-            }
-        }
-
-    }
-
-
-    public static void main(String[] args) {
-        input();
-        rec_func(1, nums[1]);
-        sb.append(max).append(" ").append(min);
-        System.out.println(sb.toString());
-    }
-
-    static class FastReader{
-        BufferedReader br;
+    static int N; // 연산 할 값의 갯수
+    static int [] A; // 연산 할 값들
+    static int [] operand = new int[4]; // +, -, *, / 연산의 갯수
+    static int MaxAns = -1000000001;
+    static int MinAns = 1000000001;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        public FastReader(){
-            br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        // 연산 할 값들 받기
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        // 사용할 수 있는 연산자 갯수 받기
+        for(int i = 0; i < 4; i++){
+            operand[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, A[0]);
+        System.out.println(MaxAns);
+        System.out.println(MinAns);
+
+    }
+
+    // 우선순위 없이 앞에서부터 진행
+    // 순열
+    static void dfs(int depth, int number){
+        // 연산은 N - 1개 만큼 한 경우 Max값과 Min 값 갱신
+        if(depth == N - 1){
+            MaxAns = Math.max(MaxAns, number);
+            MinAns = Math.min(MinAns, number);
 
         }
-        String next(){
-            while(st == null || !st.hasMoreElements()){
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+
+        // 4개의 연산을 보면서
+        for(int i = 0; i < 4; i++){
+            // 해당 연산이 남았을 경우
+            if(operand[i] > 0){
+                // 연산의 횟수를 빼주고
+                operand[i]--;
+                dfs(depth + 1, cal(number, A[depth + 1], i));
+                operand[i]++;
             }
-            return st.nextToken();
         }
-        int nextInt(){
-            return Integer.parseInt(next());
+    }
+
+    static int cal(int num1, int num2, int operand){
+        // 더하기 일 때
+        if(operand == 0){
+            return num1 + num2;
+        }
+        // 빼기일 떄
+        else if(operand == 1){
+            return num1 - num2;
+        }
+        // 곱하기 일 때
+        else if(operand == 2){
+            return num1 * num2;
+        }
+        // 나누기 일 때
+        else{
+            return num1 / num2;
         }
     }
 }
