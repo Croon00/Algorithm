f32bec90bc0236d3a08bd182fd0f439939135899
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2022-12-20 16:05:20 +0900

    [Silver IV] Title: 국영수, Time: 636 ms, Memory: 62872 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,83 +1,74 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
+    /*
+        국어/ 점수 / 영어 로 과목을 받아서
+        1. 국어 (감소)
+        2. 국어 같을 경우 영어 (증가)
+        3. 위 2 개 같을 경우 수학 (감소)
+        순서대로 정렬하자.
+     */
 
-    static class Elements implements Comparable<Elements>{
-        public String name;
-        public int ko, en, math;
+    // 비교를 위한 Comparable
+    static class Subject implements Comparable<Subject>{
+        String name;
+        int korean;
+        int math;
+        int english;
 
         @Override
-        public int compareTo(Elements o) {
-            if(o.ko == this.ko){
-                if(this.en == o.en){
+        public int compareTo(Subject o) {
+            if(this.korean == o.korean){
+                if(this.english == o.english){
                     if(this.math == o.math){
+                        // 점수가 모두 같으면 이름 사전순으로
                         return name.compareTo(o.name);
                     }
+                    // 수학은 내림 차순으로
                     return o.math - this.math;
                 }
-                return this.en - o.en;
+                // 영어는 오름 차순으로
+                return this.english - o.english;
             }
-            else{
-                return o.ko - this.ko;
-            }
-
-
-        }
-    }
-    static int N;
-    static Elements[] a;
-    static void input(){
-        FastReader fr = new FastReader();
-        Elements e = new Elements();
-        N = fr.nextInt();
-        a = new Elements[N];
-        for(int i = 0; i < N; i++){
-            a[i] = new Elements();
-            a[i].name = fr.next();
-            a[i].ko = fr.nextInt();
-            a[i].en = fr.nextInt();
-            a[i].math = fr.nextInt();
+            // 국어는 내림 차순으로
+            return o.korean - this.korean;
         }
     }
 
-    static void sort(){
-        Arrays.sort(a);
-        for (int i = 0; i <a.length; i++){
-            sb.append(a[i].name).append('\n');
-        }
-    }
-
-
-    public static void main(String[] args) {
-        input();
-        sort();
-        System.out.println(sb.toString());
-
-    }
-
-    static class FastReader{
-        BufferedReader br;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        public FastReader(){
-            br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
 
-        }
-        String next(){
-            while(st == null || !st.hasMoreElements()){
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
+        int N = Integer.parseInt(st.nextToken());
+
+        // N명의 점수
+        Subject [] student = new Subject[N];
+
+        // 받기
+        for(int i = 0;  i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            student[i] = new Subject();
+            student[i].name = st.nextToken();
+            student[i].korean = Integer.parseInt(st.nextToken());
+            student[i].english = Integer.parseInt(st.nextToken());
+            student[i].math = Integer.parseInt(st.nextToken());
         }
 
-        int nextInt(){
-            return Integer.parseInt(next());
+        Arrays.sort(student);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++){
+            sb.append(student[i].name);
+            sb.append('\n');
         }
+
+        System.out.println(sb);
     }
 }
