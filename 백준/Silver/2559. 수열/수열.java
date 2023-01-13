5d0d57600dd89adb27e6349f232dbe457151a645
MistyMoon128 <73871364+MistyMoon128@users.noreply.github.com>
2023-01-14 00:39:47 +0900

    [Silver III] Title: 수열, Time: 1004 ms, Memory: 22936 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,38 +1,55 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static int N; // 1 ~ 10만
+    static int K; // 1 ~ N
 
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] days = new int[N];
+    static int [] num;
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			days[i] = Integer.parseInt(st.nextToken());
-		}
 
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < N + 1 - K; i++) {	// K 개씩 묶어서 모두 더한 뒤 최댓값 구하기
-			int sum = 0;
-			for (int j = i; j < i + K; j++) {
-				sum += days[j];
-			}
-			if (max < sum) {
-				max = sum;
-			}
-		}
 
-		bw.write(String.valueOf(max));
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        num = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        // 주어진 수는 -100 ~ 100 이고 N은 2 이상 10만 이하임으로 최소 값이 -1000만 임으로 -10000001로 둔다.
+        int ans = -10000001;
+
+        // 두 포인터 이용
+        for(int L = 0; L < N; L++){
+            if(L + K > N) break;
+            // 0부터 시작
+            int sum = 0;
+            // 날짜 세기
+            int cnt = 0;
+            // R은 L부터
+            int R = L;
+
+            // R 의 값이 범위를 넘지 않으면서 K번 날짜 이하 세었을 때는 계속 더하기
+            while (cnt < K && R < N){
+                sum += num[R++];
+                cnt++;
+            }
+
+            // K번 까지 날자를 세었을 때만
+            if(cnt == K) ans = Math.max(ans, sum);
+        }
+        System.out.println(ans);
+    }
 }
