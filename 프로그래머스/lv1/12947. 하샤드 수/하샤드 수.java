8e0cd3d1ad24eefcbe75afa8925ed10312bebcbe
나비매듭 <73871364+Croon00@users.noreply.github.com>
2023-06-15 16:39:07 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean solution(int x) {
+        
+        boolean answer = true;
+        
+        String num = String.valueOf(x);
+        
+        int sum = 0;
+        for(int i = 0; i < num.length(); i++){
+            sum += num.charAt(i) - '0';
+            
+        }
+        if(x % sum == 0){
+            answer = true;
+        }
+        else{
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
