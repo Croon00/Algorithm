090126aced4ad61c259a066c9c5ab9c5a6ae3c22
나비매듭 <73871364+Croon00@users.noreply.github.com>
2023-07-27 15:44:20 +0900

    [level 2] Title: 숫자의 표현, Time: 1.06 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1; // 자기 자신
+        for(int i = 1; i <= n / 2; i++){
+            int sum = i;
+            for(int j = i + 1; j < n; j++){
+                sum += j;
+                if(sum == n){
+                    answer++;
+                    break;
+                }
+                else if(sum > n){
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
