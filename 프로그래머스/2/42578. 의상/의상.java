e41123d8a70409f933bfc0986c5ea8d9dd4149e1
Hyeon Seungmin <73871364+Croon00@users.noreply.github.com>
2023-10-19 00:00:16 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,22 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String, Integer> clotheHashMap = new HashMap<>();
-        
-        for(String [] clothe : clothes){
-            if(clotheHashMap.containsKey(clothe[1])){
-                int temp = clotheHashMap.get(clothe[1]);
-                clotheHashMap.put(clothe[1], temp + 1);
-            }else{
-                clotheHashMap.put(clothe[1], 2);
-            }
-        }
-        
-        for(String k : clotheHashMap.keySet()){
-            answer *= clotheHashMap.get(k);
-        }
-        answer -= 1;
+        int answer = 0;
         return answer;
     }
 }
