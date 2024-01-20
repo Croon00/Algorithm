0d1780140bcaca90ae95b00f4d70134ed613e82b
Hyeon Seungmin <73871364+Croon00@users.noreply.github.com>
2024-01-20 15:44:07 +0900

    [level 2] Title: N-Queen, Time: 151.07 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -10,56 +10,48 @@ class Solution {
         // 그 다음 행 부터 끝 행 까지
         // 같은 열이면 x, 대각 선이면 x
         
-        board = new boolean[n][n];
+        // n은 열, 들어 있는 값은 행의 값
+        int [] arr = new int[n];
         
-       
-        nQueen(0, n);
+        nQueen(0, arr, n);
         
         return answer;
     }
-    static void nQueen(int depth, int n) {
-        // 끝 행에 도착 했으면 가능한 퀸의 경우
-        if(depth == n){
-            answer++;
-            return;
-        }
-        
-        // 열을 넘기면서 하나씩 여왕 놓기
-        for(int i = 0; i < n; i++){
-            // 가능 하다면 다음 depth로 넘어가기
-            if(check(depth, i, n)){
-                board[depth][i] = true;
-                nQueen(depth + 1, n);
-                board[depth][i] = false;
-            }
-        }
-    }
+   static void nQueen(int depth, int[] arr, int n){
+       // 모든 열을 채운 경우 가능한 배치 ++
+       if(depth == n){
+           answer++;
+           return;
+       }
+       
+       for(int i = 0; i < n; i++){
+           // 해당 열이 i행에 퀸이 놓일 경우
+           arr[depth] = i;
+           
+           // 가능한 위치인 경우 다음 열로 넘어가서 체크 하기
+           if(check(depth, arr)){
+               nQueen(depth + 1, arr, n);
+           }
+       }
+   }
     
-    
-    static boolean check(int depth, int col, int n){
-        
-        int left = col;
-        int right = col;
-        
-        for(int i = depth - 1; i >= 0; i--){
-            // 위에 확인
-            if(board[i][col]){
+    static boolean check(int col, int[] arr) {
+        // 0번째 열 부터 해당 열 전 까지를 확인하면서
+        for(int i = 0; i < col; i++){
+            // 해당 열에 있는 값이랑 같은 행에 위치 하고 있는지 확인
+            if(arr[col] == arr[i]){
                 return false;
             }
             
-            // 왼쪽 위 확인
-            left--;
-            if(left >= 0 && board[i][left]){
-                return false;
-            }
-            
-            // 오른쪽 위 확인
-            right++;
-            if(right < n && board[i][right]){
+            // 대각선 위 혹은 아래에 위치하고 있는지 확인
+            // 열의 차와 행의 차가 같은 경우 대각선에 위치해있는 것이다.
+            else if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
                 return false;
             }
         }
+        
         
         return true;
     }
+    
 }
